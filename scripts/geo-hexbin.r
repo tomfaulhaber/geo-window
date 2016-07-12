b298b5770b9f34abdf4f0af1d6581baec195dcbf
Tom Faulhaber <git_com@infolace.com>
2016-07-12 14:23:32 -0700

    Add day and night into the animation for a better sense of time

--- scripts/geo-hexbin.r
+++ scripts/geo-hexbin.r
@@ -3,6 +3,7 @@ library(ggplot2)
 library(ggmap)
 library(lubridate)
 library(gganimate)
+library(animation)
 
 input.file <- "/tmp/counts"
 san.francisco <- list(left=-122.55, right=-122.3549, bottom=37.7040, top=37.8324)
@@ -43,18 +44,43 @@ bbox <- function(df, frame=0.25) {
     bottom=min(df$lat)-frame)
 }
 
-map.df <- function(df, zoom=10) {
-  df.bb <- bbox(df, frame=0.05)
+# The alpha table for the hours of the day - higher is darker - tuned for
+# SF summer, cause this is just a demo
+light.table <- c(.6, .6, .6,  .6, .45, .3,
+                 .1, 0,  0,   0,  0,   0,
+                 0,  0,  0,   0,  0,   0,
+                 .1, .3, .45, .6, .6,  .6)
+
+map.df <- function(df, zoom=10, df.bb=bbox(df, frame=0.05)) {
   hexagons <- df %>% group_by(id) %>% do(hexagon(1/240,.$lon,.$lat,.$id)) %>% ungroup
   datapoly <- merge(df, hexagons, by=c("id"))
+  base.time <- df$time[1]
+  df.hour <- hour(base.time)
   map <- get_stamenmap(df.bb, zoom = zoom, maptype = "terrain-background")
   p <- ggmap(map) + 
     geom_polygon(aes(x=x, y=y, fill=count, alpha=count, 
-                     group=id, frame=strftime(time, "%B %d %H:%M")), 
+                     group=id), 
                  data=datapoly, color="white") +
     theme(axis.title=element_blank()) +
     scale_fill_gradient(low="#9ECAE1", high="#08519C", trans="log") +
     scale_alpha_continuous(range=c(0.6,1), trans="log") +
-    guides(fill=FALSE, alpha=FALSE)
-  gg_animate(p)
+    guides(fill=FALSE, alpha=FALSE) + 
+    annotate("rect", xmin=df.bb['left'], xmax=df.bb['right'], ymin=df.bb['bottom'], ymax=df.bb['top'], color="blue", alpha=light.table[df.hour+1]) +
+    ggtitle(strftime(base.time, "%B %d %H:%M"))
+    
+  p
+}
+
+# Draw map for each hour in the data
+loop.animate <- function(df, ...) {
+  hours <- base::unique(df$time)
+  df.bb <- bbox(df, frame=0.05)
+  maps <- lapply(hours, function(h) {
+    df.hour <- subset(df, time==h)
+    print(map.df(df.hour, df.bb=df.bb))
+  })
+  
+  saveGIF(for(h in hours){
+    print(map.df(subset(df, time==h), df.bb=df.bb))
+  }, interval = .2, movie.name="sf-tweets.gif", ...)
 }
